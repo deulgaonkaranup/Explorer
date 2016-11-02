65b751986cc1b7613b6838aaf150d0422989a56d
gmathew1990 <george.mathew1990@gmail.com>
2016-11-02 01:15:44 -0500

    MySQLDataStoreUtilities and User was added

---
+++ explorer/MySQLDataStoreUtilities.java
@@ -0,0 +1,86 @@
+package explorer;
+import java.util.*;
+import java.sql.*;
+
+
+public class MySQLDataStoreUtilities
+{
+
+	public static Connection getConnection()
+	{
+
+
+		Connection conn=null;
+			try{
+
+					Class.forName("com.mysql.jdbc.Driver").newInstance();
+	  			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/explorer?useSSL=true","root","root");
+
+	 			}
+	 catch (Exception e)
+	 {
+
+	 }
+return conn;
+	}
+
+
+
+	public static void UserUpdate(User user)
+	{
+
+		try{
+	Connection conn=getConnection();
+	 conn.createStatement().executeUpdate("Insert into usr_acc values("+user.user_id+",\""+user.fname+"\",\""+user.lname+"\",\""+user.contact+"\",\""+user.email_id+"\")");
+	 conn.createStatement().executeUpdate("Insert into usr_regist values("+user.user_id+",\""+user.username+"\",\""+user.pass+"\")");
+
+			}
+			catch (Exception e)
+			{}
+
+	}
+
+public static boolean userValidation(String username)
+{
+	ResultSet rs=null;
+	boolean ret=false;
+	try{
+Connection conn=getConnection();
+int cnt=0;
+ rs = conn.createStatement().executeQuery("select username from usr_regist where username=\""+username+"\"" );
+while((rs.next())&&(cnt==0))
+{
+	cnt++;
+
+}
+if (cnt==0)
+{
+	ret=true;
+}
+		}
+		catch (Exception e)
+		{}
+	return ret;
+}
+
+public static int userID()
+{
+	ResultSet rs=null;
+	int id=0;
+	try{
+Connection conn=getConnection();
+ rs = conn.createStatement().executeQuery("select max(user_id) user_id from usr_regist" );
+while(rs.next())
+{
+	id=rs.getInt("user_id");
+}
+id++;
+
+		}
+		catch (Exception e)
+		{}
+	return id;
+}
+
+
+}
